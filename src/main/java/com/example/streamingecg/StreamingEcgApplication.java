b754bd5c8759b4a802dac89376cbb47093cb6aa4
Bartosz Kita <bartosz.kita@roche.com>
2018-10-25 12:51:51 +0200

    Project generated from Spring Initializer

---
+++ src/main/java/com/example/streamingecg/StreamingEcgApplication.java
@@ -0,0 +1,12 @@
+package com.example.streamingecg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingEcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingEcgApplication.class, args);
+	}
+}
